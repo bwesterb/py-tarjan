f47a6d0e3a1c0c5b689aa414f40b504e4abdf5cc
Bas Westerbaan <bas@westerbaan.name>
2014-08-04 17:35:03 +0200

    py26: fix test_tc

--- src/tests/test_tc.py
+++ src/tests/test_tc.py
@@ -6,7 +6,7 @@ class TestMain(unittest.TestCase):
         # see doc/example.png
         g = {1:[2],2:[1,5],3:[4],4:[3,5], 5:[6],6:[7],7:[8],8:[6,9],9:[]}
         res = tarjan.tc.tc(g)
-        res = {k: frozenset(res[k]) for k in res}
+        res = dict([(k, frozenset(res[k])) for k in res])
         self.assertEqual(res, {1: frozenset([1, 2, 5, 6, 7, 8, 9]),
                                2: frozenset([1, 2, 5, 6, 7, 8, 9]),
                                3: frozenset([3, 4, 5, 6, 7, 8, 9]),
