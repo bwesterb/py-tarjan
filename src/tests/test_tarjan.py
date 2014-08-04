9db25a3ccede333ae53d2ead689cbd144ef7fc0d
Bas Westerbaan <bas@westerbaan.name>
2014-08-04 14:57:49 +0200

    py3: fix test

--- src/tests/test_tarjan.py
+++ src/tests/test_tarjan.py
@@ -6,11 +6,11 @@ class TestMain(unittest.TestCase):
         # see doc/example.png
         g = {1:[2],2:[1,5],3:[4],4:[3,5], 5:[6],6:[7],7:[8],8:[6,9],9:[]}
         for f in (tarjan.tarjan, tarjan.tarjan_recursive):
-            res = map(frozenset, f(g))
-            self.assertTrue(res == map(frozenset,
-                                [[9], [8,7,6], [5], [2,1], [4,3]]) or
-                            res == map(frozenset,
-                                [[9], [8,7,6], [5], [4,3], [2,1]]))
+            res = list(map(frozenset, f(g)))
+            self.assertTrue(res == list(map(frozenset,
+                                [[9], [8,7,6], [5], [2,1], [4,3]])) or
+                            res == list(map(frozenset,
+                                [[9], [8,7,6], [5], [4,3], [2,1]])))
 
 if __name__ == '__main__':
     unittest.main()
